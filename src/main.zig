b72dcc7cfaa7a2e485cb353c653f61f7ac763372
Sam Atman <atmanistan@gmail.com>
2024-10-08 23:46:33 -0400

    Add raw mode support to demo main executable

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,41 @@
-//! Main Executable of termread
+//! Test executable.  Sets up raw mode, reads, prints.
+//!
+//! This is not intended to be durable or robust, just to
+//! take in inputs and display them.
+
 const std = @import("std");
 
-test "exe mentioned" {
-    std.debug.print("hello from main\n", .{});
+const posix = std.posix;
+
+// Setup code borrow from libvaxis: https://github.com/rockorager/libvaxis/blob/main/src/posix/Tty.zig
+
+/// makeRaw enters the raw state for the terminal.
+pub fn makeRaw(fd: posix.fd_t) !posix.termios {
+    const state = try posix.tcgetattr(fd);
+    var raw = state;
+    // see termios(3)
+    raw.iflag.IGNBRK = false;
+    raw.iflag.BRKINT = false;
+    raw.iflag.PARMRK = false;
+    raw.iflag.ISTRIP = false;
+    raw.iflag.INLCR = false;
+    raw.iflag.IGNCR = false;
+    raw.iflag.ICRNL = false;
+    raw.iflag.IXON = false;
+
+    raw.oflag.OPOST = false;
+
+    raw.lflag.ECHO = false;
+    raw.lflag.ECHONL = false;
+    raw.lflag.ICANON = false;
+    raw.lflag.ISIG = false;
+    raw.lflag.IEXTEN = false;
+
+    raw.cflag.CSIZE = .CS8;
+    raw.cflag.PARENB = false;
+
+    raw.cc[@intFromEnum(posix.V.MIN)] = 1;
+    raw.cc[@intFromEnum(posix.V.TIME)] = 0;
+    try posix.tcsetattr(fd, .FLUSH, raw);
+    return state;
 }
